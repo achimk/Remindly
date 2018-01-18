032a2c29e7b82b2f6ec269b6f7009353c0ea4659
Joachim Kret <joachim.kret@esky.com>
2018-01-18 15:01:06 +0100

    Added Data Source module.

---
+++ Infrastructure/DataSource/DataSource.h
@@ -0,0 +1,19 @@
+//
+//  DataSource.h
+//  DataSource
+//
+//  Created by Joachim Kret on 18.01.2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DataSource.
+FOUNDATION_EXPORT double DataSourceVersionNumber;
+
+//! Project version string for DataSource.
+FOUNDATION_EXPORT const unsigned char DataSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataSource/PublicHeader.h>
+
+
---
+++ Infrastructure/DataSource/DataSource.swift
@@ -0,0 +1,11 @@
+//
+//  DataSource.swift
+//  DataSource
+//
+//  Created by Joachim Kret on 18.01.2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+
+public typealias DataSource<T> = Array<T>
