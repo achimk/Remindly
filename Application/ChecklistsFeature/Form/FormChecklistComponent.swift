d7f704987b766e66bc602e1a921e9cb72600f328
Joachim Kret <joachim.kret@esky.com>
2018-01-22 15:32:03 +0100

    Prepared empty form for checklist.

---
+++ Application/ChecklistsFeature/Form/FormChecklistComponent.swift
@@ -0,0 +1,15 @@
+//
+//  FormChecklistComponent.swift
+//  ChecklistsFeature
+//
+//  Created by Joachim Kret on 22.01.2018.
+//  Copyright © 2018 JK. All rights reserved.
+//
+
+import Foundation
+import Reminders
+import Gears
+
+final class FormChecklistComponent {
+    
+}
